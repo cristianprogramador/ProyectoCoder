d21eb4a04618df647440b7ebf9badf4f3bc7ec20
Cristian Cerda <cristiandrivedatos@gmail.com>
2023-02-08 18:21:32 -0300

    guardado08022023

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,10 +5,26 @@ from AppCoder.models import Curso
 
 # Create your views here.
 
-def curso(self):
+#def curso(self):
 
-    curso = Curso(nombres='Backend', camada='12345')
+    #curso = Curso(nombres='Backend', camada='12345')
     #curso.save()
-    documentoDeTexto = f'--->Curso: {curso.nombres} camada: {curso.camada}'
-    return HttpResponse(documentoDeTexto)
+    #documentoDeTexto = f'--->Curso: {curso.nombres} camada: {curso.camada}'
+    #return HttpResponse(documentoDeTexto)
+
+def inicio(request):
+    return HttpResponse('vista inicio')
+
+def curso(request):
+    return HttpResponse('vista curso')
+
+def profesores(request):
+    return HttpResponse('vista profesores')
+
+def estudiantes(request):
+    return HttpResponse('vista estudiantes')
+
+def entregables(request):
+    return HttpResponse('vista entregable')
+    
 
